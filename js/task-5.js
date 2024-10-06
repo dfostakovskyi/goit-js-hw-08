8471f855231256204519504fd7dcb7569ff2badb
Dfostakovskyi <d.fostakovskiy@gmail.com>
2024-10-05 17:15:27 -0700

    05.10.24.17-15

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,16 @@
+const jsBody = document.querySelector("body");
+const jsButton = document.querySelector(".change-color");
+
+jsButton.addEventListener("click", changeColor);
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function changeColor(event) {
+  jsBody.style.backgroundColor = getRandomHexColor();
+   
 }
